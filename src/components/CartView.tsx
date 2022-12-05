167b9ea5a68704d3ea66c311ff9c71cfffa58914
Ioan Dragomir <123ioandragomir123@gmail.com>
2022-12-05 15:06:03 +0200

    Change payment interface to be in the sell page instead of the scanned page

--- src/components/CartView.tsx
+++ src/components/CartView.tsx
@@ -1,23 +1,44 @@
 import { useStore } from '@nanostores/preact';
 import { cartItems, removeFromCart, disableCart, disableCartFlag } from '../cartStore.ts';
 import { useState, useEffect } from 'preact/hooks';
+import { post } from '../api.ts';
 import './CartView.css';
 
 export default function CartView() {
 	const $cartItems = useStore(cartItems);
 	const [status, setStatus] = useState('Pay');
+	const [modal, setModal] = useState(null);
 
-	useEffect(_ => {delete localStorage['checkout'];}, [])
+	function scanned() {
+		if(!(localStorage.scanTime > Date.now() - 5000)) return;
+
+		window.removeEventListener('storage', scanned);
+
+		const id = localStorage.scan;
+		delete localStorage.scan;
+		delete localStorage.scanTime;
+
+		post('checkout', {
+			user: id,
+			iids: Object.values($cartItems).map(({ item, quantity }) => item.id),
+			qtys: Object.values($cartItems).map(({ item, quantity }) => quantity),
+		}).then(({ message, prev_balance, current_balance }) => {
+			setModal(<><h1>Accepted!</h1><p>{message}</p><p>Previous balance: {prev_balance}</p><p>Current balance: {current_balance}</p></>);
+		}, ({ error, current_balance }) => {
+			setModal(<><h1>Error!</h1><p>{error}</p><p>Balance: {current_balance}</p></>);
+		});
+	}
 
 	function checkout() {
 		disableCart();
-		localStorage['checkout'] = JSON.stringify(Object.values($cartItems));
 		setStatus('Scan to pay');
+		localStorage.needScan = true;
+		window.addEventListener('storage', scanned);
 	}
 
 	let total = 0;
 
-	return (
+	return <>
 		<div id="cart-view">
 			<h1>Cart</h1>
 			<div id="cart-items">
@@ -36,5 +57,9 @@ export default function CartView() {
 			</div>
 			<button type="submit" disabled={!Object.keys($cartItems).length || disableCartFlag} onClick={checkout}>{status} {total}</button>
 		</div>
-	);
+		{ modal ? <><div id="checkout-modal">
+			{ modal }
+			<a href='#' onclick={_=>window.location.reload()}>Ok, refresh</a>
+		</div><div id="checkout-modal-backdrop"></div></> : '' }
+	</>;
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,14 +1,19 @@
-import { get } from '../api.ts';
+import { get, post } from '../api.ts';
 import { useState, useEffect } from 'preact/hooks';
 
 export default function UserCard({ id }) {
 	const [tag, setTag] = useState(null);
+	const [topUpProgress, setTopUpProgress] = useState('');
 	if(!id) id = window.location.search.substr(1);
 
-	useEffect(_ => {
+	function getTag() {
 		get(`tag/${encodeURIComponent(id)}`).then(tag => {
 			setTag(tag)
 		});
+	}
+
+	useEffect(_ => {
+		getTag();
 	}, []);
 
 	if(tag === null) {
@@ -18,8 +23,27 @@ export default function UserCard({ id }) {
 		return <p class='error'>No data found</p>
 	}
 
+	function doTopUp(tagId, amount) {
+		setTopUpProgress('Topping up...');
+
+		post(`tag/${tagId}/topUp`, {
+			'amount': amount
+		}).then(_ => {
+			setTopUpProgress('Top-up OK!');
+			getTag();
+		}).catch(e => {
+			setTopUpProgress(e);
+		})
+	}
+
 	return (<>
 		<h1>{tag.name}</h1>
 		<p>Balance: {tag.balance}</p>
+		{ localStorage.authLevel && localStorage.authLevel.includes('TAGS_TOPUP') ?
+		<form id='top-up' onSubmit={e=>{e.preventDefault(); doTopUp(id, document.querySelector('#top-up-amount').value)}}>
+			<h1>Top up</h1>
+			<input id='top-up-amount' type='number' placeholder='Token count' value='0' />
+			<button type='submit' disabled={!!topUpProgress}>{!topUpProgress ? 'Add tokens' : topUpProgress}</button>
+		</form> : '' }
 	</>);
 }
